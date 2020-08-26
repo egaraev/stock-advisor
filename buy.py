62dfc72e98a530fd791cd6b9867b74b83ceb5fa9
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2020-08-26 21:43:56 +0200

    Update buy.py

--- buy.py
+++ buy.py
@@ -193,12 +193,10 @@ def telegram_bot_sendtext(bot_message):
    bot_token = telegram_bot_token
    bot_chatID = telegram_bot_chatID
    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-
    response = requests.get(send_text)
+   return response.json()
 
-   return response
-	
-	
+
 def Mail(FROM,TO,SUBJECT,TEXT,SERVER):
 
 # Prepare actual message
