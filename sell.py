091814bb04c8d854098cfa9516ad7570e762b105
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2020-08-31 22:13:27 +0200

    Update sell.py

--- sell.py
+++ sell.py
@@ -63,7 +63,8 @@ def Sell():
           hour_candle_direction=market_values(market,21)
           tweet_positive=market_values(market,8)
           tweet_negative=market_values(market,9)
-          tweet_ratio = tweet_positive/tweet_negative
+          tweet_ratio = float("{0:.4f}".format(tweet_positive/tweet_negative)
+          print (tweet_ratio)
           danger_order=status_orders(market, 29)
           
           ai_price=market_values(market,11)
