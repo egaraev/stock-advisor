27b6eed91b1326d752fd1ca5d32350200ab44dae
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2020-08-31 16:41:18 +0200

    Update sell.py

--- sell.py
+++ sell.py
@@ -242,7 +242,7 @@ def Sell():
 
                  if  procent_serf<=-15.0:
                       print ('    4 -Selling ' + str(format_float(sell_quantity_sql)) + ' units of ' + market + ' for ' + str(format_float(last)) + '  and getting  ' + str(format_float(serf)) + ' USD')
-                      printed = ('    Fuck, we fucked up, we reached our Stop Loss, so to avoid complete disaster lets sell all this shit  ' + market + ' for this crappy price' + str(format_float(last)) + '  and lose  ' + str(format_float(procent_serf)) + ' % . Fuck fuck!! ' +' For more details go here: http://139.162.132.189')
+                      #printed = ('    Fuck, we fucked up, we reached our Stop Loss, so to avoid complete disaster lets sell all this shit  ' + market + ' for this crappy price' + str(format_float(last)) + '  and lose  ' + str(format_float(procent_serf)) + ' % . Fuck fuck!! ' +' For more details go here: http://139.162.132.189')
                       try:
 
                           db = pymysql.connect("localhost", "stockuser", "123456", "stock_advisor")
@@ -262,9 +262,9 @@ def Sell():
                       finally:
                           db.close()
                       #Mail("egaraev@gmail.com", "egaraev@gmail.com", "New sell, SL", printed, "localhost")
-                      send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + printed
-                      response = requests.get(send_text)
-                      print (response.json())
+                      #send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + printed
+                      #response = requests.get(send_text)
+                      #print (response.json())
 
           else:
               pass 
