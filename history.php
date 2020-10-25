607203763e9fdbee6b6c58319acd559d92ecdb8c
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2020-10-25 09:58:47 +0100

    Update history.php

--- history.php
+++ history.php
@@ -7,7 +7,7 @@ if (mysqli_connect_errno())
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$result = mysqli_query($con,"SELECT * FROM history order by symbol");
+$result = mysqli_query($con,"SELECT * FROM history order by date DESC, symbol");
 
 
 echo "<table border='1'>
