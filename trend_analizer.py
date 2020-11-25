915dfd2e8b31e62f455303f5140d1acaf34b0986
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2020-11-25 17:14:20 +0100

    Update trend_analizer.py

--- trend_analizer.py
+++ trend_analizer.py
@@ -38,6 +38,8 @@ def TA():
           fourdaysbeforeprice = (price[4][0])
           fivedaysbeforeprice = (price[5][0])
           prices= [currentprice, daybeforeprice, twodaysbeforeprice, threedaysbeforeprice, fourdaysbeforeprice, fivedaysbeforeprice]
+          percent_change = currentprice/min(prices)*100
+          print (percent_change)
           if (currentprice==max(prices)):
              print ("Peak")
              trend = "Peak"
