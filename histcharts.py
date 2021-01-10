c9c4cb651a5bcf40b4454b4f3ed77eb388fd5342
Eldar Garayev <32615582+egaraev@users.noreply.github.com>
2021-01-10 09:30:35 +0100

    Update histcharts.py

--- histcharts.py
+++ histcharts.py
@@ -33,7 +33,7 @@ def SL():
           print (symbol)
           db = pymysql.connect("localhost", "stockuser", "123456", "stock_advisor")
           cursor = db.cursor()
-          cursor.execute("SELECT date, twitter_polarity, twitter_score, news_score, price, predicted_price, positive_tweets, negative_tweets, candle_score FROM history WHERE symbol='%s' ORDER BY date DESC LIMIT 70" % symbol)
+          cursor.execute("SELECT date, twitter_polarity, twitter_score, news_score, price, predicted_price, positive_tweets, negative_tweets, candle_score FROM history WHERE symbol='%s' ORDER BY date DESC LIMIT 200" % symbol)
           data=cursor.fetchall()
 
           df = pd.DataFrame(data)
